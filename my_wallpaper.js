56048eeeb81235d90f76815fd709bb865f3151f6
Willowsummers <willowsummers04@gmail.com>
2025-07-22 11:38:34 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
-
+var wheelSize = 22 //22
+var rectCorner = 4 //4
+var wheelHeightX = 130 //130
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -17,9 +17,33 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(242, 215, 155); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+  //rect(40 ,40, rect_width, rect_height);
+
+
+    // Car body
+    strokeWeight(1)
+   fill(255, 0, 0); // red
+  rect(40, 100, 120, 30, rectCorner*4, rectCorner+2, rectCorner, rectCorner);
+
+  // Car top
+  strokeWeight(1)
+  fill(167, 238, 252,); // light blue
+  rect(65, 80, 90, 25, rectCorner*4, rectCorner*7, rectCorner, rectCorner);
+
+  // Wheels
+  fill(0); // black
+  ellipse(60, wheelHeightX, wheelSize);  // front wheel
+  ellipse(140, wheelHeightX, wheelSize); // rear wheel
+
+//Hub caps 
+fill(200); // black
+  ellipse(60, wheelHeightX, wheelSize/1.5);  // front wheel
+  ellipse(140, wheelHeightX, wheelSize/1.5); // rear wheel
+
+
 }
