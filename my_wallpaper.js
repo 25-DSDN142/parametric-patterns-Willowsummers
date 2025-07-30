255003d97f338338cd936a82f9ec2d3ec205310a
Willowsummers <willowsummers04@gmail.com>
2025-07-30 17:50:54 +1200

    having trouble with making colour variables work

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,13 @@
 //your parameter variables go here!
-var smallstroke = 0.4
-var bigstroke = 1
+
+// Color variables
+
+let redCol = (190, 30, 45);
+let yellow = (255,212,23);
+let blue = (33, 64, 154);
+let white = (255);
+let black = (0);
+
 
 function setup_wallpaper(pWallpaper) {
 pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -12,66 +19,69 @@ pWallpaper.output_mode(DEVELOP_GLYPH);
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 150;
+  pWallpaper.grid_settings.row_offset  = 100;
+
+
+
+
 }
 
 function wallpaper_background() {
   background(230, 223, 204); //Beige 
 }
-
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-fill(190, 30, 45,) //Red 
+
+
+fill(190, 30, 45)
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
  triangle (55, 90, 95, 160, 55, 140)
+
+  fill(255, 212, 23) //Yellow
+stroke(0)  
+strokeWeight(0.4)
+rect(140,120,50,70)
 
 fill(33, 64, 154) //Blue
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
   square(95, 80, 80)
 
-noFill()
+fill(255)
 stroke(0)  
-strokeWeight(smallstroke)
-rect(140,120,50,70)
-
-
-fill(255, 212, 23,) //Yellow
-stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
 arc(55, 90, 80, 70, 270, 420, PIE);
 
-noFill()
+ fill(33, 64, 154) //Blue
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
 arc(175, 160, 30, 30, 270, 180, PIE)
 
 fill(255, 212, 23) //Yellow
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
  rect(10, 10, 45, 80);
 
  fill(33, 64, 154) //Blue
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
   ellipse(94, 25, 100, 30)
 
 fill(255, 212, 23,) //Yellow
 square(160, 145, 15)
 
-noFill()
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
 fill(190, 30, 45) //Red 
 arc(135, 80, 80 , 80, 0, 180, PIE )
 fill(255, 212, 23,) //Yellow
 arc(95, 160, 90, 100, 270, 0, PIE)
 
 
-noFill()
+fill(255, 212, 23,) //Yellow
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
 arc(55, 140, 40, 40, 386, 270, PIE)
 
 fill(190, 30, 45,) //Red 
@@ -83,11 +93,10 @@ vertex(160, 40)
 vertex(95, 80)
 endShape()
 
- fill(33, 64, 154) //Blue
+ fill(0)
 stroke(0)  
-strokeWeight(smallstroke)
+strokeWeight(0.4)
 circle(155, 63, 33)
-
 
 
 }
