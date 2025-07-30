ce8e4cf9312af1b13e4fe2017ccec0aec8c911a2
Willowsummers <willowsummers04@gmail.com>
2025-07-31 11:09:55 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,11 +2,13 @@
 
 let ss = 0.4 // 0.4
 
-let triX = 35; //55
+// Parameters for function DrawPacEatTri
+let triX =55; //55
 let triY = 60; //90
 
-let squareX = 95; //95
-let squareY = 80; //80
+// Parameters for function DrawFunkySquare
+let squareX = 50; //95
+let squareY = 50; //80
 
 let rectX = 140;
 let rectY = 120;
@@ -50,24 +52,27 @@ function wallpaper_background() {
 }
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-DrawPacEatTri()
+//DrawPacEatTri()
 
 DrawFunkySquare()
+
+
+
 
 fill(white)
 stroke(0)  
 strokeWeight(ss)
-arc(55, 90, 80, 70, 270, 420, PIE);
+//arc(55, 90, 80, 70, 270, 420, PIE);
 
 fill(255, 212, 23) //Yellow
 stroke(0)  
 strokeWeight(ss)
-rect(10, 10, 45, 80);
+//rect(10, 10, 45, 80);
 
  fill(33, 64, 154) //Blue
 stroke(0)  
 strokeWeight(ss)
-  ellipse(94, 25, 100, 30)
+ // ellipse(94, 25, 100, 30)
 
 
 
@@ -79,12 +84,12 @@ vertex(85, 65)
 vertex(150, 25)
 vertex(160, 40)
 vertex(95, 80)
-endShape()
+//endShape()
 
  fill(0)
 stroke(0)  
 strokeWeight(ss)
-circle(155, 63, 33)
+//circle(155, 63, 33)
 
 }
 
@@ -108,21 +113,21 @@ rect(squareX + 45, squareY +40 , 50, 70)
 
 fill(blue) 
 square(squareX, squareY, 80)
-
 arc(squareX + 80 , squareY + 80, 30, 30, 270, 180, PIE)
 
 fill(yellow)
 square(squareX + 65, squareY + 65, 15)
 
-
 fill(red) 
 arc(squareX + 40,squareY , 80 , 80, 0, 180, PIE )
-
 
 fill(yellow) 
 arc(squareX , squareY + 80, 90, 100, 270, 0, PIE)
 
-
-
+noFill()
+stroke(black)
+strokeWeight(ss)
+rect(squareX , squareY , 95, 110)
+arc( squareX, squareY + 110, 60, 60 , 270, 0, PIE)
 
 }
