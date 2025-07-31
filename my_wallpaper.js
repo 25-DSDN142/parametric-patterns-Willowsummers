13ef781157702ee5ccb14402858a34e93a582c0b
Willowsummers <willowsummers04@gmail.com>
2025-07-31 18:09:34 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,7 @@
 //your parameter variables go here!
 
-let ss = 0.4 // 0.4
+//strokeWeight 
+let ss = 0.4 // 0.4 thin
 
 // Parameters for function DrawPacEatTri
 let triX =55; //55
@@ -10,18 +11,13 @@ let triY = 60; //90
 let squareX = 50; //95
 let squareY = 50; //80
 
-let rectX = 140;
-let rectY = 120;
-let rectW = 50;
-let rectH = 70;
+//Parameters for DrawBAU
+let BAUX = 30//20
+let BAUY = -14//20
 
-let arcw = 80;
-let arcH = 70;
-
-let ellipseW = 100;
-let ellipseH = 30; 
-
-let circleSize = 33;
+//Parameters for DrawHAUS
+let HAUSX = 10//40
+let HAUSY = 153 //40
 
 // Color variables
 let red = [190, 30, 45];
@@ -29,84 +25,43 @@ let yellow = [255,212,23];
 let blue = [33, 64, 154];
 let white = [255];
 let black = [0];
-
+let beige = [230, 223, 204]
 
 function setup_wallpaper(pWallpaper) {
 pWallpaper.output_mode(DEVELOP_GLYPH);
- //pWallpaper.output_mode(GRID_WALLPAPER);
+//pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 100;
+  pWallpaper.grid_settings.row_offset  = 0;
 
 
 
 }
 
 function wallpaper_background() {
-  background(230, 223, 204); //Beige 
+  background(beige)
 }
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-//DrawPacEatTri()
 
-DrawFunkySquare()
+DrawFunkySquare();
+DrawCorners();
+//DrawBAU();
+DrawHAUS();
 
-
-
-
-fill(white)
-stroke(0)  
-strokeWeight(ss)
-//arc(55, 90, 80, 70, 270, 420, PIE);
-
-fill(255, 212, 23) //Yellow
-stroke(0)  
-strokeWeight(ss)
-//rect(10, 10, 45, 80);
-
- fill(33, 64, 154) //Blue
-stroke(0)  
-strokeWeight(ss)
- // ellipse(94, 25, 100, 30)
-
-
-
-
-fill(190, 30, 45,) //Red 
-beginShape()
-vertex(95, 80)
-vertex(85, 65)
-vertex(150, 25)
-vertex(160, 40)
-vertex(95, 80)
-//endShape()
-
- fill(0)
-stroke(0)  
-strokeWeight(ss)
-//circle(155, 63, 33)
-
+push();
+translate(200, 100); // move to bottom right
+scale(-1, -1);       // flip both axes
+DrawBAU();
+pop();
 }
 
-
-
-
-
-
-
-
-
-
-
-
 //Functions
-
-
 
 function DrawPacEatTri(){
 
@@ -144,5 +99,70 @@ stroke(black)
 strokeWeight(ss)
 rect(squareX , squareY , 95, 110)
 arc( squareX, squareY + 110, 60, 60 , 270, 0, PIE)
+}
+
+function DrawCorners(){
+
+noStroke()
+fill(blue)
+arc(0,200, 60, 60 , 270, 0, PIE)
+arc(200,200, 60, 60 , 180, 270, PIE)
+arc(200,0, 60, 60 , 90, 180, PIE)
+arc(0,0, 60, 60 , 360, 90, PIE)
 
 }
+
+
+function DrawBAU(){
+
+  noStroke();
+  fill(0);
+  // B
+  rect(BAUX - 2, BAUY + 30, 6, 30);
+  arc(BAUX + 6, BAUY + 38, 16, 16, PI + HALF_PI, HALF_PI, CHORD);
+  arc(BAUX + 6, BAUY + 52, 16, 16, PI + HALF_PI, HALF_PI, CHORD); 
+
+  // A
+  triangle(BAUX + 15, BAUY + 60, BAUX + 27, BAUY + 30, BAUX + 39, BAUY + 60);
+  fill(beige);
+  triangle(BAUX + 25, BAUY + 60, BAUX + 27, BAUY + 56, BAUX + 29, BAUY + 60);
+  
+  // U
+  fill(black);
+  rect(BAUX + 41, BAUY + 30, 6, 24);
+  rect(BAUX + 53, BAUY + 30, 6, 24);
+  arc(BAUX + 50, BAUY + 53, 18, 18, 0, 180);
+
+}
+
+function DrawHAUS(){
+noStroke();
+  fill(black);
+
+  // H
+  rect(HAUSX + 70, HAUSY + 10, 6, 30);
+  rect(HAUSX + 82, HAUSY + 10, 6, 30);
+  rect(HAUSX + 76, HAUSY + 20, 6, 6);
+
+  // A
+  triangle(HAUSX + 90, HAUSY + 40, HAUSX + 102, HAUSY + 10, HAUSX + 114, HAUSY + 40);
+  fill(beige);
+  triangle(HAUSX + 100, HAUSY + 40, HAUSX + 102, HAUSY + 36, HAUSX + 104, HAUSY + 40);
+ 
+  // U
+   fill(black);
+  rect(HAUSX + 115, HAUSY + 10, 6, 24);
+  rect(HAUSX + 127, HAUSY + 10, 6, 24);
+  arc(HAUSX + 124, HAUSY + 33, 18, 18, 0, 180);
+
+  // S
+  arc(HAUSX + 145, HAUSY + 18, 18, 18, 90, 360);
+  arc(HAUSX + 145, HAUSY + 32, 18, 18, 270, 180 )
+
+}
+
+
+
+
+
+
